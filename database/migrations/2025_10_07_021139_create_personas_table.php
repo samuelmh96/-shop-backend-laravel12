2c627ce425b13dd454420e2b403066bb2a6aaeb1
Samuel Medrano <samu.96.h@gmail.com>
2025-10-06 22:32:25 -0400

    feat: modelos Persona y Documento con migraciones

---
+++ database/migrations/2025_10_07_021139_create_personas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres', 50);
+            $table->string('apellidos', 50);
+            $table->date('fecha_nacimiento')->nullable();
+            $table->string('estado')->default(true);
+            $table->integer('ci')->nullable();
+
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
