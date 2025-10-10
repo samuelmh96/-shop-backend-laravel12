a7afdaedce462f7b8f19d74199e598c908e40060
Samuel Medrano <samu.96.h@gmail.com>
2025-10-10 08:50:01 -0400

    migrations and relations of table M:N

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocumentoController;
+use App\Http\Controllers\PermisoController;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +13,21 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+    // Rutas para usuarios
 Route::middleware('auth:sanctum')->group(function () {
+    // CRUD API REST USUARIOS
     Route::apiResource('/users', UserController::class);
+
+    // CRUD ROLES
+    Route::apiResource('role', RoleController::class);
+    Route::apiResource('persona', PersonaController::class);
+    Route::apiResource('permiso', PermisoController::class);
+    Route::apiResource('documento', DocumentoController::class);
+
 });
 
+
+ // Rutas para autenticación
 Route::prefix('/v1/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
